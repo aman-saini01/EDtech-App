69546f8415ff1bf7f1bbb3554fdf54230a96200e
aman-saini01 <as9047845@gmail.com>
2024-07-28 12:00:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		  origin: 'http://localhost:3000',
+		  origin: 'https://edtech-app-git-main-aman-saini01s-projects.vercel.app',
 		credentials: true,
 	})
 );
